be7940a21c217b5c767ec0293723ed04829a9a22
AnithaVedantam <35854306+anithavedantam@users.noreply.github.com>
2024-09-10 20:10:56 -0700

    Time: 2271 ms (26.10%), Space: 66.9 MB (9.53%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -1,25 +1,27 @@
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
-        rows, cols = len(grid1), len(grid1[0])
+        ROWS,COLS = len(grid1), len(grid1[0])
         visited = set()
+        
         def dfs(r,c):
-            if (r < 0 or r == rows or 
-               c < 0 or c == cols or
-               grid2[r][c] == 0 or (r,c) in visited):
+            if (r < 0 or c < 0 or r == ROWS or c == COLS or
+                grid2[r][c] == 0 or (r,c) in visited):
                 return True
-            visited.add((r,c))
+            
             res = True
-            if (grid1[r][c] == 0):
+            if grid1[r][c] == 0:
                 res = False
+            
+            visited.add((r,c))
             res = dfs(r+1, c) and res
             res = dfs(r-1, c) and res
             res = dfs(r, c+1) and res
             res = dfs(r, c-1) and res
             return res
+            
         count = 0
-        for r in range(rows):
-            for c in range(cols):
-                if grid2[r][c] and (r,c) not in visited and dfs(r,c):
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (grid2[r][c] and (r,c) not in visited and dfs(r,c)):
                     count += 1
         return count
-        

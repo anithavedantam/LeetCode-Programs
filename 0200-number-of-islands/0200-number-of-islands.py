5d3e727bef81f1b6a201cf13bd7e941c24db21e0
AnithaVedantam <35854306+anithavedantam@users.noreply.github.com>
2024-09-10 18:24:33 -0700

    Time: 311 ms (13.66%), Space: 24.4 MB (23.67%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,33 +2,31 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
-        
+        visit = set()
         islands = 0
         rows, cols = len(grid), len(grid[0])
-        visited = set()
         
         def bfs(r,c):
             q = collections.deque()
-            directions = [[0,1], [0,-1], [1,0], [-1,0]]
+            visit.add((r,c))
             q.append((r,c))
             
             while q:
                 row, col = q.popleft()
+                directions =[[1,0], [0,1], [-1,0], [0,-1]]
+                
                 for dr, dc in directions:
-                    r = row + dr
-                    c = col + dc
-                    if (r in range(rows) and 
+                    r, c = row + dr, col + dc
+                    if (r in range(rows) and
                         c in range(cols) and
-                        grid[r][c] == "1" and 
-                        (r,c) not in visited):
-                        visited.add((r,c))
+                        (r,c) not in visit and
+                        grid[r][c] == "1"):
                         q.append((r,c))
-                        
-                            
+                        visit.add((r,c))
+                    
         for r in range(rows):
             for c in range(cols):
-                if (r,c) not in visited and grid[r][c] == "1":
+                if grid[r][c] == "1" and (r,c) not in visit:
                     bfs(r,c)
                     islands += 1
         return islands
-        
